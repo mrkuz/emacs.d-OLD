8ddb1d65c5065bdcf0139738f8fa3ffce15fef57
mrkuz <markus@bitsandbobs.net>
2021-10-16 21:35:03 +0200

    Add orderless

--- modules/completion.el
+++ modules/completion.el
@@ -1,5 +1,3 @@
-(setq completion-styles '(basic substring flex))
-
 ;; Ignore case on completion
 (setq read-file-name-completion-ignore-case t)
 (setq read-buffer-completion-ignore-case t)
@@ -9,6 +7,12 @@
 (use-package vertico
   :config
   (vertico-mode))
+
+;; Use orderless completion style
+(use-package orderless
+  :config
+  (setq orderless-matching-styles '(orderless-flex))
+  (setq completion-styles '(orderless)))
 
 ;; Show extra information (like description, file permission, ...) next to minibuffer completions
 (use-package marginalia
