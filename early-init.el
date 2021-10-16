9403ac7ac819e7ae48c5d40241f6aeb4f5add106
mrkuz <markus@bitsandbobs.net>
2021-10-16 19:13:24 +0200

    Add no-littering

--- early-init.el
+++ early-init.el
@@ -3,4 +3,4 @@
 
 ;; Move gccemacs eln cache to var
 (when (boundp 'native-comp-eln-load-path)
-  (add-to-list 'native-comp-eln-load-path (concat user-emacs-directory "var/eln-cache")))
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "var/eln-cache" user-emacs-directory)))
--- init.el
+++ init.el
@@ -1,2 +1,11 @@
 ;; Load modules
-(load-file (concat user-emacs-directory "modules/straight.el"))
+(load-file (expand-file-name "modules/straight.el" user-emacs-directory))
+
+;; Keep ~/.emacs.d clean by using 'etc' and 'var' subdirectories
+(use-package no-littering
+  :init
+  (setq no-littering-etc-directory (expand-file-name "etc/" user-emacs-directory))
+  (setq no-littering-var-directory (expand-file-name "var/" user-emacs-directory))
+  :config
+  (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  (setq custom-file (no-littering-expand-etc-file-name "custom.el")))
