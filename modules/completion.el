00e0573abda24e69f151a67a726c5330a180bc67
mrkuz <markus@bitsandbobs.net>
2021-10-16 20:31:56 +0200

    Add marginalia

--- modules/completion.el
+++ modules/completion.el
@@ -8,3 +8,8 @@
 (use-package vertico
   :config
   (vertico-mode))
+
+;; Show extra information (like description, file permission, ...) next to minibuffer completions
+(use-package marginalia
+  :config
+  (marginalia-mode))
