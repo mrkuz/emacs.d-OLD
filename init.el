322858db5382f27238481928c26ca4a719d4cb4b
mrkuz <markus@bitsandbobs.net>
2021-10-16 20:29:11 +0200

    Move no-littering to own module

--- init.el
+++ init.el
@@ -3,17 +3,10 @@
 
 ;; Load modules
 (my/load-module "straight")
+(my/load-module "no-littering")
 (my/load-module "appearance")
 (my/load-module "completion")
 (my/load-module "keymap")
-
-;; Keep ~/.emacs.d clean by using 'etc' and 'var' subdirectories
-(use-package no-littering
-  :config
-  (setq no-littering-etc-directory (expand-file-name "etc/" user-emacs-directory))
-  (setq no-littering-var-directory (expand-file-name "var/" user-emacs-directory))
-  (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-  (setq custom-file (no-littering-expand-etc-file-name "custom.el")))
 
 ;; Show initialization time on startup
 (add-hook 'emacs-startup-hook
