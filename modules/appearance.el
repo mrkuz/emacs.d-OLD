14e7445da02f2734e01ce041e3d5b0ace7fac37f
mrkuz <markus@bitsandbobs.net>
2021-10-16 22:30:47 +0200

    More appearance tweaks

--- modules/appearance.el
+++ modules/appearance.el
@@ -1,5 +1,5 @@
 ;; Set up initial frame position and size
-(setq initial-frame-alist '((width . 140) (height . 50)))
+(setq default-frame-alist '((width . 140) (height . 50)))
 
 ;; Disable toolbar
 (tool-bar-mode 0)
@@ -11,20 +11,23 @@
 (scroll-bar-mode 0)
 ;; Show matching parens
 (show-paren-mode 1)
-;; Show line numbers
-(global-display-line-numbers-mode 1)
+
 ;; Show column number in mode line
 (column-number-mode 1)
+;; Show line numbers
+(add-hook 'prog-mode-hook (lambda () (display-line-numbers-mode)))
+
 
 ;; Show date in modeline
 (setq display-time-default-load-average nil)
-(setq display-time-format "%Y-%m-%d W%V")
+(setq display-time-format "%Y/%m/%d W%V")
 (display-time-mode 1)
 
 ;; Show buffer name in frame title
 (setq-default frame-title-format '("Emacs - %b"))
 ;; Set font
 (set-frame-font "Ubuntu Mono:size=16")
+;; (set-frame-font "DejaVu Sans Mono:size=14")
 
 ;; Load theme
 (use-package dracula-theme
