8ad4cb0f13520c305bdf5efa7993ec0d2de88d31
mrkuz <markus@bitsandbobs.net>
2021-10-17 20:20:06 +0200

    Update orderless styles

--- modules/completion.el
+++ modules/completion.el
@@ -11,8 +11,8 @@
 ;; Use orderless completion style
 (use-package orderless
   :config
-  (setq orderless-matching-styles '(orderless-flex))
-  (setq completion-styles '(orderless)))
+  (setq orderless-matching-styles '(orderless-literal orderless-regexp orderless-flex)
+        completion-styles '(orderless)))
 
 ;; Show extra information (like description, file permission, ...) next to minibuffer completions
 (use-package marginalia
