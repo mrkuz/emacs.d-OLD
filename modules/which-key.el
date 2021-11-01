bc2efa20598752ac80aa52165b6b8881c638b686
mrkuz <markus@bitsandbobs.net>
2021-11-01 14:20:55 +0100

    Move which-key window to the right side

--- modules/which-key.el
+++ modules/which-key.el
@@ -1,5 +1,11 @@
 ;; Show available key bindings based on your current input
 (use-package which-key
   :diminish which-key-mode
+  :init
+  (setq which-key-show-docstrings t
+        which-key-max-description-length 50
+        which-key-max-display-columns 1
+        which-key-side-window-max-width 0.5
+        which-key-side-window-location 'right)
   :config
   (which-key-mode 1))
