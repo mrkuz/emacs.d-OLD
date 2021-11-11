8ebbbc6adc04873638c3832d0548321280075476
mrkuz <markus@bitsandbobs.net>
2021-11-11 09:45:04 +0100

    Configure scrolling

--- config/behavior.el
+++ config/behavior.el
@@ -53,6 +53,17 @@
       completion-ignore-case t)
 
 ;;--------------------------------------------------------------------------------------------------
+;; Scrolling
+;;--------------------------------------------------------------------------------------------------
+
+;; Move cursor to first/last line when hit top/bottom
+(setq scroll-error-top-bottom t)
+;; Keep cursor position when scrolling pages
+(setq scroll-preserve-screen-position t)
+;; Try to keep 5 lines visible when scrolling
+(setq scroll-step 5)
+
+;;--------------------------------------------------------------------------------------------------
 ;; Projects
 ;;--------------------------------------------------------------------------------------------------
 
